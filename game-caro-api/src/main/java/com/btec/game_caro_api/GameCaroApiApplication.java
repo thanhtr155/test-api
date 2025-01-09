1006743cf619b06d6039740919e03cee9e3208cc
Trần Tuấn Thành <152388776+thanhtr155@users.noreply.github.com>
2025-01-09 07:53:24 +0700

    first commit
    first commit api demo

---
+++ game-caro-api/src/main/java/com/btec/game_caro_api/GameCaroApiApplication.java
@@ -0,0 +1,13 @@
+package com.btec.game_caro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCaroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCaroApiApplication.class, args);
+	}
+
+}
---
+++ game-caro-api/src/test/java/com/btec/game_caro_api/GameCaroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btec.game_caro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCaroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
